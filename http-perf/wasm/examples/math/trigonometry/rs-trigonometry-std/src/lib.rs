27cbf5f76d5d22b383356aa111b9c9ab2ab9d903
yanagitani <takanori.yanagitani@gmail.com>
2023-06-06 07:46:03 +0900

    feat: more functions

--- http-perf/wasm/examples/math/trigonometry/rs-trigonometry-std/src/lib.rs
+++ http-perf/wasm/examples/math/trigonometry/rs-trigonometry-std/src/lib.rs
@@ -2,3 +2,13 @@
 pub extern "C" fn f64_sin(x: f64) -> f64 {
     x.sin()
 }
+
+#[no_mangle]
+pub extern "C" fn f32_sin(x: f32) -> f32 {
+    x.sin()
+}
+
+#[no_mangle]
+pub extern "C" fn f64_atan(x: f64) -> f64 {
+    x.atan()
+}
