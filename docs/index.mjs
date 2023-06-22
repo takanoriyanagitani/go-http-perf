17b80f13ecc3f6aaa28bb6fdd9ffe685a94cb3fa
yanagitani <takanori.yanagitani@gmail.com>
2023-06-23 08:23:08 +0900

    docs: soft link -> copy

--- docs/index.mjs
+++ docs/index.mjs
@@ -1,1 +1,25 @@
-../http-perf/wasm/examples/math/trigonometry/rs-sin-fast/index.mjs
+(() => {
+  const fast = document.getElementById("fast");
+  const slow = document.getElementById("slow");
+
+  const fms = document.getElementById("fast-ms");
+  const sms = document.getElementById("slow-ms");
+
+  const sel = document.getElementById("calls");
+
+  const fw = new Worker("fast.worker.js");
+  fw.onmessage = (evt) => {
+    const { data } = evt;
+    const { ms } = data;
+    fms.textContent = ms;
+  };
+  fast.onclick = (_) => fw.postMessage({ calls: sel.value - 0 });
+
+  const sw = new Worker("normal.worker.js");
+  sw.onmessage = (evt) => {
+    const { data } = evt;
+    const { ms } = data;
+    sms.textContent = ms;
+  };
+  slow.onclick = (_) => sw.postMessage({ calls: sel.value - 0 });
+})();
