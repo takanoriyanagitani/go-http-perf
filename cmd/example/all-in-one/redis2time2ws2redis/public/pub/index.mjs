6a03763a2f18b5b9f6f2ccc066cb2853ece3a14a
yanagitani <takanori.yanagitani@gmail.com>
2023-07-17 06:51:55 +0900

    docs: example with counter(send/recv)

--- cmd/example/all-in-one/redis2time2ws2redis/public/pub/index.mjs
+++ cmd/example/all-in-one/redis2time2ws2redis/public/pub/index.mjs
@@ -1,54 +1,75 @@
 (() => {
+  const _sent = document.getElementById("sent");
+  const _recv = document.getElementById("recv");
 
-	const { host } = window.location;
-	const wpath = "ws://" + host + "/redis2ws2redis";
-	console.info(wpath)
-	const ws = new WebSocket(wpath);
-	ws.binaryType = "arraybuffer";
+  const incText = (prev) => {
+    const i = prev - 0;
+    const next = i + 1;
+    return next;
+  };
 
-	const compose = (f, g) => {
-		return t => {
-			const u = f(t);
-			const v = g(u);
-			return v;
-		};
-	};
+  const incContent = (ele) => {
+    const next = incText(ele.textContent);
+    ele.textContent = next;
+  };
 
-	// TODO
-	const us2json = (micros) => {
-		return JSON.stringify({
-			dummy: "data",
-			sample: 42,
-			c: 2.99792458,
-			micros: micros + "",
-			complex: {
-				header: ["c1", "c2"],
-				data: [
-				  [2, 0.599],
-				  [3, 3.776],
-				  [5, 0.634],
-				],
-			},
-		})
-	};
+  const incSent = (_) => incContent(_sent);
+  const incRecv = (_) => incContent(_recv);
 
-	const data2us = data => {
-		const v = new DataView(data);
-		const us = v.getBigInt64(0);
-		return us;
-	};
+  const { host } = window.location;
+  const wpath = "ws://" + host + "/redis2ws2redis";
+  console.info(wpath);
+  const ws = new WebSocket(wpath);
+  ws.binaryType = "arraybuffer";
 
-	const data2us2json = compose(
-		data2us,
-		us2json,
-	);
+  const compose = (f, g) => {
+    return (t) => {
+      const u = f(t);
+      const v = g(u);
+      return v;
+    };
+  };
 
-	ws.onmessage = evt => {
-		const { data } = evt;
-		const { byteLength } = data;
-		if(byteLength < 8) return ws.send("");
-		const j = data2us2json(data);
-		ws.send(j);
-	};
+  // TODO
+  const us2json = (micros) => {
+    return JSON.stringify({
+      dummy: "data",
+      sample: 42,
+      c: 2.99792458,
+      micros: micros + "",
+      complex: {
+        header: ["c1", "c2"],
+        data: [
+          [2, 0.599],
+          [3, 3.776],
+          [5, 0.634],
+        ],
+      },
+    });
+  };
 
-})()
+  const data2us = (data) => {
+    const v = new DataView(data);
+    const us = v.getBigInt64(0);
+    return us;
+  };
+
+  const data2us2json = compose(
+    data2us,
+    us2json,
+  );
+
+  const send2ws = (dat) => {
+    ws.send(dat);
+    incSent();
+  };
+
+  ws.onmessage = (evt) => {
+    incRecv();
+    const { data } = evt;
+    const { byteLength } = data;
+    if (byteLength < 8) return send2ws("");
+    const j = data2us2json(data);
+    send2ws(j);
+  };
+})();
